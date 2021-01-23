ffcdc5b25d5014d87d69bc36bf8a7b1ca55c576f
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-01-22 22:18:05 -0500

    WIP: Push it a bit further

--- jdaviz/configs/imviz/plugins/parsers.py
+++ jdaviz/configs/imviz/plugins/parsers.py
@@ -1,5 +1,5 @@
 import base64
-#import pathlib
+import pathlib
 import uuid
 
 from jdaviz.core.registries import data_parser_registry
@@ -15,16 +15,13 @@ def imviz_image_parser(app, data, data_label=None, show_in_viewer=True):
         data_label = "imviz_data|" + str(
             base64.b85encode(uuid.uuid4().bytes), "utf-8")
 
-    # TMP: REMOVE ME AND IMPLEMENT REAL PARSER
-    import numpy as np
-    data = np.arange(10000).reshape((100, 100))
-
-    #path = pathlib.Path(data)
-    #if path.is_file():
-    #    data = load_image(...)
-    #    pass
-    #else:
-    #    raise FileNotFoundError(f"No such file: {path}")
+    path = pathlib.Path(data)
+    if path.is_file():
+        # TODO: Support other image formats
+        from astropy.io import fits
+        data = fits.getdata(path)
+    else:
+        raise FileNotFoundError(f"No such file: {path}")
 
     app.add_data(data, data_label)
     if show_in_viewer:

e671b3903689817570f520d6e12d52c5d81c6647
Brett M. Morris <bmmorris@stsci.edu>
2023-06-13 13:27:25 -0400

    tooltip improvements

--- jdaviz/components/viewer_data_select_item.vue
+++ jdaviz/components/viewer_data_select_item.vue
@@ -13,7 +13,7 @@
     </div>
 
     <j-tooltip
-      :tooltipcontent="isRefData() && viewer.config === 'imviz' ? 'Current viewer orientation' : 'Set viewer orientation'"
+      :tooltipcontent=dataMenuTooltip
       span_style="font-size: 12pt; padding-top: 6px; width: calc(100% - 80px); white-space: nowrap; cursor: default;">
     <span
       style="cursor: pointer;"
@@ -163,6 +163,15 @@ module.exports = {
         return 'black'
       } else {
         return 'gray'
+      }
+    },
+    dataMenuTooltip() {
+      if (this.$props.viewer.config === 'imviz' && this.isRefData()) {
+        return 'Current viewer orientation'
+      } else if (this.$props.viewer.config === 'imviz') {
+        return 'Set viewer orientation'
+      } else {
+        return null
       }
     }
   }
